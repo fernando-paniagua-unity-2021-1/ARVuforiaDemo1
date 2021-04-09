0785b8b8b8047558e823356cd4628c00c06886e2
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-04-09 21:08:09 +0200

    Initial commit

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour
+{
+    bool motorArrancado = false;
+    public float speed;
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.transform.gameObject.CompareTag("F1"))
+                {
+                    motorArrancado = true;
+                }
+            }
+        }
+        if (motorArrancado)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
